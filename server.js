86bc0b9ad287b075238bf081019cb25a1d523498
Sócrates Eduardo Mendes <socrates.mendes7@hotmail.com>
2025-06-11 20:09:59 -0300

    Update: Correção no pck.json

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import app from "./src/app.js";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Servidor escutando!");
+    console.log(`✅ Servidor Smart Energy rodando na porta ${PORT}`);
 });
